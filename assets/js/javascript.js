5a1b8d0d70d7974132c7d51bba673f5862f70d91
xezian <xezian@icloud.com>
2017-10-25 14:15:32 -0700

    wonderful box

---
+++ assets/js/javascript.js
@@ -0,0 +1,53 @@
+
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+var cw = canvas.width;
+var ch = canvas.height;
+
+document.getElementById("button1").addEventListener("click", function() {
+    var height = canvas.offsetHeight;
+    var width = canvas.offsetWidth;
+    var growth = height + 50;
+    var large = width + 50;
+    canvas.style.width = large + 'px';
+    canvas.style.height = growth + 'px';
+});
+
+document.getElementById("button2").addEventListener("click", function() {
+	canvas.style.opacity = 1;
+  	ctx.fillStyle = "blue";
+  	ctx.fillRect(0, 0, cw, ch);
+});
+
+document.getElementById("button3").addEventListener("click", function() {
+	canvas.style.opacity = 0.3;
+});
+
+document.getElementById("button6").addEventListener("click", function() {
+  	canvas.reset = location.reload();
+});
+
+document.getElementById("button4").addEventListener("click", function() {
+	canvas.style.opacity = 1;
+	var grd = ctx.createLinearGradient(0, 0, cw, ch);
+		grd.addColorStop(0, "red");
+		grd.addColorStop(.18, "orange");
+		grd.addColorStop(.36, "yellow");
+		grd.addColorStop(.54, "green");
+		grd.addColorStop(.69, "blue");
+		grd.addColorStop(.87, "indigo");
+		grd.addColorStop(1, "violet");
+	
+	ctx.fillStyle = grd;
+	ctx.fillRect(0, 0, cw, ch);
+
+});
+
+document.getElementById("button5").addEventListener("click", function() {
+	
+	canvas.style.opacity = 1;
+	ctx.fillStyle = "black"
+	ctx.font = "50px Arial black";
+	ctx.fillText("Wonderful!",10,50);
+
+});	
